ede1448c0578d7953d582c032da39b998f47b1e4
João Pedro <srdoog1@gmail.com>
2026-05-10 12:53:45 -0300

    código refatorado

--- src/server/app.js
+++ src/server/app.js
@@ -3,13 +3,6 @@ const express = require('express');
 const app = express();
 const pegarHtml = require('../services/scrapperService');
 
-function pegarBloco(lista, inicio, fim) {
-    const start = lista.indexOf(inicio);
-    const end = lista.indexOf(fim);
-
-    return lista.slice(start + 1, end);
-}
-
 app.get('/', async (req, res) => {
     try {
         const dados = await pegarHtml();
@@ -28,7 +21,7 @@ app.get('/', async (req, res) => {
                 opcao: dados.comidasAlmoco[1],
                 guarnicao: dados.comidasAlmoco[2],
                 acompanhamentos: dados.comidasAlmoco[3],
-                saladas: [dados.comidasAlmoco[4], dados.comidasAlmoco[5] ],
+                saladas: [dados.comidasAlmoco[4], dados.comidasAlmoco[5]],
                 sobremesa: dados.comidasAlmoco[6],
                 suco: dados.comidasAlmoco[7]
             },
@@ -38,7 +31,7 @@ app.get('/', async (req, res) => {
                 opcao: dados.comidasJantar[1],
                 guarnicao: dados.comidasJantar[2],
                 acompanhamentos: dados.comidasJantar[3],
-                saladas: [ dados.comidasJantar[4], dados.comidasJantar[5] ],
+                saladas: [dados.comidasJantar[4], dados.comidasJantar[5]],
                 sobremesa: dados.comidasJantar[6],
                 suco: dados.comidasJantar[7]
             }
@@ -46,6 +39,62 @@ app.get('/', async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/cafe', async (req, res) => {
+    try {
+        const dados = await pegarHtml();
+        res.json({
+            cafe: {
+                desjejum: dados.comidas[0],
+                acompanhamento: dados.comidas[1],
+                cafe: dados.comidas[2],
+                leite: dados.comidas[3],
+                fruta: dados.comidas[4],
+                suco: dados.comidas[5]
+            },
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+});
+
+app.get('/almoco', async (req, res) => {
+    try {
+        const dados = await pegarHtml();
+        res.json({
+            almoco: {
+                prato_principal: dados.comidasAlmoco[0],
+                opcao: dados.comidasAlmoco[1],
+                guarnicao: dados.comidasAlmoco[2],
+                acompanhamentos: dados.comidasAlmoco[3],
+                saladas: [dados.comidasAlmoco[4], dados.comidasAlmoco[5]],
+                sobremesa: dados.comidasAlmoco[6],
+                suco: dados.comidasAlmoco[7]
+            },
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+});
+
+app.get('/jantar', async (req, res) => {
+    try {
+        const dados = await pegarHtml();
+        res.json({
+            jantar: {
+                prato_principal: dados.comidasJantar[0],
+                opcao: dados.comidasJantar[1],
+                guarnicao: dados.comidasJantar[2],
+                acompanhamentos: dados.comidasJantar[3],
+                saladas: [dados.comidasJantar[4], dados.comidasJantar[5]],
+                sobremesa: dados.comidasJantar[6],
+                suco: dados.comidasJantar[7]
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
